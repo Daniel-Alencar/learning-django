ca8664fa476faf61381e66f3c7e7727d802ebc70
Daniel-Alencar <danielalencar746@gmail.com>
2021-07-12 10:43:27 -0300

    feat: added pages tasks

--- learningDjango/urls.py
+++ learningDjango/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include("app.urls")),
     path('newyear/', include("newyear.urls")),
+    path('tasks/', include("tasks.urls")),
 ]
---
+++ tasks/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+tasks = ["Alguma coisa", "Outra coisa", "Alguma outra coisa"]
+
+# Create your views here.
+def index(request):
+  return render(request, "tasks/index.html", {
+    "tasks": tasks
+  })
