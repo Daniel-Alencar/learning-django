9b04e707f5bc590541c3966bd35cff96494f1abf
Daniel-Alencar <danielalencar746@gmail.com>
2021-07-14 15:12:59 -0300

    feat:Utilizaçao de sessions para possibilitar o acesso de varios usuarios

--- tasks/views.py
+++ tasks/views.py
@@ -7,8 +7,6 @@ from django.shortcuts import render
 # reverse() nos retorna uma parte da URL especificada
 from django.urls import reverse
 
-tasks = []
-
 # Definimos uma nova classe para criação de formulário
 # Neste caso, essa classe permite criarmos um formulário com o input 'task' do tipo 'text
 class NewTaskForm(forms.Form):
@@ -16,8 +14,13 @@ class NewTaskForm(forms.Form):
 
 # Create your views here.
 def index(request):
+  # Verificamos se dentro da sessão particular temos a propriedade "tasks" (lembre-se que a session é como um grande dicionário)
+  if "tasks" not in request.session:
+    # Caso não exista, nós criamos a propriedade como uma lista vazia
+    request.session["tasks"] = []
+
   return render(request, "tasks/index.html", {
-    "tasks": tasks
+    "tasks": request.session["tasks"]
   })
 
 def add(request):
@@ -29,17 +32,19 @@ def add(request):
     if form.is_valid():
       # Pegamos a tarefa que foi especificada no formulário e colocamos à lista de tarefas 'tasks'
       task = form.cleaned_data["task"]
-      tasks.append(task)
+      request.session["tasks"] += [task]
 
       # Redirecionamos o usuário para a rota definida com o nome de 'index' de nossa aplicação 'tasks' (no arquivo urls.py)
       # reverse() nos retorna a parte da URL especificada, neste caso, '/tasks'
       return HttpResponseRedirect(reverse("tasks:index"))
     else:
-      # Renderizamos o mesmo arquivo add.html, mas com o mesmo formulário que foi preenchido. Assim, é possível visualizar todos os erros cometidos no preenchimento
+      # Renderizamos o mesmo arquivo add.html, mas com o mesmo formulário que foi preenchido. 
+      # Assim, é possível visualizar todos os erros cometidos no preenchimento
       return render(request, "tasks/add.html", {
         "form": form
       })
-  # Caso o método seja GET, significa que o usuário apenas tentou obter a página em vez de enviar dados a ela. Assim, enviamos apenas um formulário vazio
+  # Caso o método seja GET, significa que o usuário apenas tentou obter a página em vez de enviar dados a ela. 
+  # Assim, enviamos apenas um formulário vazio
   return render(request, "tasks/add.html", {
     "form": NewTaskForm()
   })
